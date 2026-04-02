79c3612862120d352e96b3ac4ea0766071df13ce
donghuaiyu5-ui <donghuaiyu5@gmail.com>
2026-04-02 14:10:39 -0400

    Update camera.js

--- LunchQuiz/camera.js
+++ LunchQuiz/camera.js
@@ -1,1 +1,18 @@
+let video;
+
+function setup() {
+  createCanvas(640, 480);
+
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+}
+
+function draw() {
+  translate(width, 0);
+  scale(-1, 1);
+
+  image(video, 0, 0, width, height);
+}
+
 
